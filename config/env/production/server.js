8b186f63b1239fc5c765abc5a31a3087c5bd65fc
xkazm04 <kazdan@seznam.cz>
2022-04-24 12:35:21 +0200

    Config updated

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('Yhttps://d3v-center.herokuapp.com/'),
+    url: env('https://d3v-center.herokuapp.com/'),
     app: { 
       keys: env.array(process.env.APP_KEYS)
     },
