32f72e34ab91defcb651d86173c85d5b4f26f272
xkazm04 <kazdan@seznam.cz>
2022-05-10 09:49:47 +0200

    Definition schema

---
+++ src/api/definition/routes/definition.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * definition router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::definition.definition');
