7acd2d6e7228cd1499028a5e5d3f5bddc7a7d13a
xkazm04 <kazdan@seznam.cz>
2022-06-04 14:28:22 +0200

    Upgraded version

---
+++ src/lifecyclesDiscarded.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+module.exports = {
+  
+    async afterCreate(event) {
+      const { params } = event;
+
+
+  
+      // do something to the result;
+    },
+  };
